266b7f98e80c6724b75f537a03c252948f50b613
Gordon Freeman <fernando.llovel.alfaro@gmail.com>
2019-11-17 23:08:05 -0300

    paquetex(propio) eliminado crear un archivo case en productos . nuevos modelos: region y comuna

--- mantenedorUsuarios/models.py
+++ mantenedorUsuarios/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import User
 
+class Region(models.Model):
+    nombre = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.nombre
+class  Comuna(models.Model):
+    nombre = models.CharField(max_length=30)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.nombre
 
 REGION_CHOICES = [( 0, 'Región Metropolitana de Santiago'), ( 1, 'Región de Tarapacá'), (2, 'Región de Antofagasta'), (3, 'Región de Atacama'), (4, 'Región de Coquimbo'), (5, 'Región de Valparaíso'), (6, 'Región del Libertador Bernardo O \'Higgins'), (7, 'Región del Maule'), (8, 'Región del Bío Bío'), (9, 'Región de la Araucanía'), (10, 'Región de los Lagos'), (11, 'Región de Aysén del General Carlos Ibáñez del Campo'), (12, 'Región de Magallanes y la Antártica Chilena'), (13, 'Región de Los Ríos'), (14, 'Región de Arica-Parinacota')]
 
@@ -19,8 +28,10 @@ class Perfil(models.Model):
     run = models.CharField(max_length=15)
     telefono = PhoneNumberField()
     fecha_nacimiento = models.DateField(null=True)
-    region = models.IntegerField(choices=REGION_CHOICES ,null=True)
-    comuna = models.CharField(choices=COMUNA_CHOICES, max_length=60 , null=True )
+    region = models.ForeignKey(Region , on_delete=models.SET_NULL , null=True)
+    comuna = models.ForeignKey(Comuna , on_delete=models.SET_NULL , null=True)
     vivienda = models.IntegerField(choices=VIVENDA_CHOICES , null=True)
+
+
 
 
